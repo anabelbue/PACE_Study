80475077aaca24ace12e9a448d4d144fba8dc894
anabelbue <82653533+anabelbue@users.noreply.github.com>
2024-10-21 15:08:53 +0200

    Update analyze_ESM.R

--- analyze_ESM.R
+++ analyze_ESM.R
@@ -2,7 +2,6 @@
 
 analyze_ESM <- function(dat, var, min_diff, max_diff) {
   library(tidyverse)
-devtools::install_github("Sciurus365/fitlandr") 
 library(fitlandr)
 
   dat <- dat[, c("created", var)]
